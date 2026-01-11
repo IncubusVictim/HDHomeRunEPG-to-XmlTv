ff4494e8dddffc111e0e8ec87714fa85e0bfcd97
Eric Partridge <partridge.eric@frontiernet.net>
2026-01-10 21:00:12 -0500

    Specified python path and days flag

--- run_tvguide.sh
+++ run_tvguide.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python /app/HDHomeRunEPG_To_XmlTv.py --host "$HOST" --filename "$FILENAME" >> /app/output/cron.log 2>&1
+/usr/local/bin/python /app/HDHomeRunEPG_To_XmlTv.py --host "$HOST" --filename "$FILENAME" --days "$DAYS" >> /app/output/cron.log 2>&1
 echo "TV guide updated at $(date)" >> /app/output/cron.log
